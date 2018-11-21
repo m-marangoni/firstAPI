3b25c41f807f4464b7a97e41394394437b59378b
m-marangoni <marangoni.mariana@gmail.com>
2018-11-21 21:14:18 -0200

    put

--- firstapi.js
+++ firstapi.js
@@ -2,7 +2,6 @@ const express = require('express');
 const users = require('./users.js');
 const app = express();
 
-
 app.use(express.json());
 
 app.get('/', (req, res) => res.send([1, 2, 3]));
@@ -24,5 +23,21 @@ app.post('/api/users', (req, res) => {
     users.push(newUser);
     res.send(newUser);
 });
+
+app.put('/api/users/:id', (req, res) => {
+    const updateUser = users.find(user => user.id === parseInt(req.params.id));
+    if (!updateUser) {
+        return res.status(404).send('deu merda');
+    }
+    if (!req.body.name || !req.body.email) {
+        return res.status(400).send('precisa incluir nome e email!');
+    }
+
+     updateUser.name = req.body.name;
+     updateUser.email = req.body.email;
+
+   res.send(updateUser);
+}); 
+   
 
 app.listen(3000, () => console.log('ouvindo na porta 3000...'));
